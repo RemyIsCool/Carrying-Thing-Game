485013fc4946f69675b7b12b673f2aba0bb9fccb
Remy Fouquette <remyfouquette@icloud.com>
2022-10-19 19:49:00 -0700

    added fall

--- FallingPlatform/FallingPlatform.gd
+++ FallingPlatform/FallingPlatform.gd
@@ -2,9 +2,10 @@ extends StaticBody2D
 
 
 func _physics_process(delta: float) -> void:
-  if $PlayerDetector.overlaps_body(GlobalNodes.player) and not $FallTimer.time_left > 0:
-	  $FallTimer.start()
+	print($PlayerDetector.overlaps_body(GlobalNodes.player))
+	if $PlayerDetector.overlaps_body(GlobalNodes.player) and not $FallTimer.time_left > 0:
+		$FallTimer.start()
 
 
 func _on_FallTimer_timeout() -> void:
-  queue_free()
+	$AnimationPlayer.play("crumble")
