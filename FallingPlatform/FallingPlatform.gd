616bdc55bb6a2004c4820baf0715e0aa45557286
Remy Fouquette <remyfouquette@icloud.com>
2022-11-05 14:23:25 -0700

    started level 1

--- FallingPlatform/FallingPlatform.gd
+++ FallingPlatform/FallingPlatform.gd
@@ -1,13 +1,11 @@
 extends StaticBody2D
 
 
+export var gravity := 10
 
-func _physics_process(delta: float) -> void:
-	if $PlayerDetector.overlaps_body(GlobalNodes.player) and $FallTimer.time_left <= 0:
-		$FallTimer.start()
+onready var start_position := position
 
-func _on_AppearTimer_timeout() -> void:
-	$AnimationPlayer.play("RESET")
+var fallen := false
 
 # Never gonna give you up
 # Never gonna let you down
@@ -16,7 +14,33 @@ func _on_AppearTimer_timeout() -> void:
 # Never gonna say goodbye
 # Never gonna tell a lie and hurt you
 
+export var velocity := Vector2.ZERO
 
-func _on_FallTimer_timeout() -> void:
-	if $PlayerDetector.overlaps_body(GlobalNodes.player):
-		$AnimationPlayer.play("crumble")
+func _physics_process(delta: float) -> void:
+	if not fallen:
+		if $CollisionDetector.overlaps_body(GlobalNodes.player):
+			fallen = true
+		return
+
+	velocity.y += gravity
+
+	position += velocity * delta
+
+
+func _on_CollisionDetector_body_entered(body: Node) -> void:
+	if body.is_in_group("platform"):
+		fallen = false
+		velocity = Vector2.ZERO
+		$Timer.start()
+		$Sprite.visible = false
+		$CollisionShape2D.disabled = true
+		$Particles.restart()
+
+func _on_Timer_timeout() -> void:
+	fallen = false
+	position = start_position
+	velocity = Vector2.ZERO
+	$Sprite.visible = true
+	$CollisionShape2D.disabled = false
+	$Particles.restart()
+
