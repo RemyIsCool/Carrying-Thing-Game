8078924a17d7a8bd89b9b98143dd89037d8d8270
Remy Fouquette <remyrfouquette@gmail.com>
2022-11-11 07:17:31 -0800

    door open wit box and fix tileset and fix level 1

--- Door/Door.gd
+++ Door/Door.gd
@@ -4,6 +4,6 @@ extends Area2D
 export var next_level_path: String
 
 func _physics_process(delta: float) -> void:
-	if overlaps_body(GlobalNodes.player):
+	if overlaps_body(GlobalNodes.player) and GlobalNodes.player.holding:
 		SceneLoader.change_scene(next_level_path)
 		$CollisionShape2D.disabled = true
